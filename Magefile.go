3cbb516d2d292472206aef520f782efc459b02db
Mahendra Paipuri <mahendra.paipuri@gmail.com>
2024-01-17 14:05:30 +0100

    build: Set up a build target in magefile
    Signed-off-by: Mahendra Paipuri <mahendra.paipuri@gmail.com>

--- Magefile.go
+++ Magefile.go
@@ -6,7 +6,16 @@ package main
 import (
 	// mage:import
 	build "github.com/grafana/grafana-plugin-sdk-go/build"
+	"github.com/magefile/mage/mg"
 )
 
+// Build is copied from Grafana's SDK (build package), because the
+// Arrow dependency doesn't support Linux ARM in the build matrix. I've removed
+// it from the listing here.
+func Build() {
+	b := build.Build{}
+	mg.Deps(b.Linux, b.Windows, b.Darwin, b.DarwinARM64, b.LinuxARM64, b.LinuxARM)
+}
+
 // Default configures the default target.
-var Default = build.BuildAll
+var Default = Build
