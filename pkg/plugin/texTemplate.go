43cd9f5450b6b77b62db65801c5bfd1ad2ab7215
Mahendra Paipuri <mahendra.paipuri@gmail.com>
2024-01-22 13:50:43 +0100

    chore: Use recommended config for renderer
    * Update go.mod
    
    Signed-off-by: Mahendra Paipuri <mahendra.paipuri@gmail.com>

--- pkg/plugin/texTemplate.go
+++ pkg/plugin/texTemplate.go
@@ -5,49 +5,63 @@ const (
 %use square brackets as golang text templating delimiters
 \documentclass{article}
 \usepackage{graphicx}
-\usepackage[margin=1in]{geometry}
+[[- if .IsLandscapeOrientation]] 
+	\usepackage[landscape,margin=0.1in]{geometry} 
+[[- else]] 
+	\usepackage[margin=0.1in]{geometry} 
+[[- end]] 
 
 \graphicspath{ {images/} }
 \begin{document}
-\title{[[.Title]] [[if .VariableValues]] \\ \large [[.VariableValues]] [[end]] [[if .Description]] \\ \small [[.Description]] [[end]]}
-\date{[[.FromFormatted]]\\to\\[[.ToFormatted]]}
+
+\title{
+	[[.Title]] 
+	[[- if .VariableValues]] 
+		\\ \large [[.VariableValues]] 
+	[[- end]] 
+	[[- if .Description]] 
+		\\ \small [[.Description]] 
+	[[- end]]
+}
+
+\date{
+	[[.From]]\\
+	to\\
+	[[.To]]
+}
+
 \maketitle
+
 \begin{center}
-[[range .Panels]][[if .IsSingleStat]]\begin{minipage}{0.3\textwidth}
-\includegraphics[width=\textwidth]{image[[.Id]]}
-\end{minipage}
-[[else]]\par
-\vspace{0.5cm}
-\includegraphics[width=\textwidth]{image[[.Id]]}
-\par
-\vspace{0.5cm}
-[[end]][[end]]
 
-\end{center}
-\end{document}
-`
-
-	defaultGridTemplate = `
-%use square brackets as golang text templating delimiters
-\documentclass{article}
-\usepackage{graphicx}
-\usepackage[margin=0.5in]{geometry}
-
-\graphicspath{ {images/} }
-\begin{document}
-\title{[[.Title]] [[if .VariableValues]] \\ \large [[.VariableValues]] [[end]] [[if .Description]] \\ \small [[.Description]] [[end]]}
-\date{[[.FromFormatted]]\\to\\[[.ToFormatted]]}
-\maketitle
-\begin{center}
-[[range .Panels]][[if .IsPartialWidth]]\begin{minipage}{[[.Width]]\textwidth}
-\includegraphics[width=\textwidth]{image[[.Id]]}
-\end{minipage}
-[[else]]\par
-\vspace{0.5cm}
-\includegraphics[width=\textwidth]{image[[.Id]]}
-\par
-\vspace{0.5cm}
-[[end]][[end]]
+[[- if .IsGridLayout]]
+	[[- range .Panels]]
+		[[- if .IsPartialWidth]]
+			\begin{minipage}{[[.Width]]\textwidth}
+			\includegraphics[width=\textwidth]{image[[.Id]]}
+			\end{minipage}
+		[[- else]]\par
+			\vspace{0.5cm}
+			\includegraphics[width=\textwidth]{image[[.Id]]}
+			\par
+			\vspace{0.5cm}
+		[[- end]]
+		%
+	[[- end]]
+[[else]]
+	[[- range .Panels]]
+		[[- if .IsSingleStat]]
+			\begin{minipage}{0.3\textwidth}
+			\includegraphics[width=\textwidth]{image[[.Id]]}
+			\end{minipage}
+		[[- else]]\par
+			\vspace{0.5cm}
+			\includegraphics[width=\textwidth]{image[[.Id]]}
+			\par
+			\vspace{0.5cm}
+		[[- end]]
+	[[- end]]
+[[- end]]
 
 \end{center}
 \end{document}
