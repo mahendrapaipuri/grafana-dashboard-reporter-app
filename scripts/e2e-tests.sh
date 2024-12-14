bdd327c812730ecc5ca010672ba28001844b0294
Mahendra Paipuri <mahendra.paipuri@gmail.com>
2024-12-14 23:02:04 +0100

    test: Setup e2e tests for permission checks
    * New e2e tests will check inherited permissions on dashboards
    
    Signed-off-by: Mahendra Paipuri <mahendra.paipuri@gmail.com>

---
+++ scripts/e2e-tests.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Constants
+API_PATH="api/plugins/mahendrapaipuri-dashboardreporter-app/resources/report"
+DASH_UID="fdlwjnyim1la8f"
+
+# CLI ARGS
+VARIANT="$1"
+
+if [[ "$VARIANT" == "plain" ]]; then
+    GRAFANA_PROTOCOL="http"
+    GRAFANA_PORT="3080"
+    QUERY_PARAMS="layout=simple&orientation=portrait&dashboardMode=default&var-testvar0=All&var-testvar1=foo&var-testvar2=1"
+    ALT_DASH_UID="fe6xfyecry03ke"
+else
+    GRAFANA_PROTOCOL="https"
+    GRAFANA_PORT="3443"
+    QUERY_PARAMS="layout=grid&orientation=landscape&dashboardMode=full&from=now-5m&to=now&var-testvar0=All&var-testvar1=foo&var-testvar2=1"
+    ALT_DASH_UID="be6xghyd5cf0gb"
+fi
+
+# Tests
+echo "Making basic report generation request by admin"
+curl -k -o "$VARIANT-admin.pdf" "$GRAFANA_PROTOCOL://admin:admin@localhost:$GRAFANA_PORT/$API_PATH?dashUid=$DASH_UID&$QUERY_PARAMS"
+
+echo "Making request by user from team with permission on dashboard"
+curl -k -o "$VARIANT-teamuser.pdf" "$GRAFANA_PROTOCOL://teamuser:teamuser@localhost:$GRAFANA_PORT/$API_PATH?dashUid=$ALT_DASH_UID"
+
+echo "Making request by normal user without permission on dashboard"
+RESP_CODE=$(curl -k -s -o /dev/null -w "%{http_code}" "$GRAFANA_PROTOCOL://normaluser:normaluser@localhost:$GRAFANA_PORT/$API_PATH?dashUid=$ALT_DASH_UID")
+
+# Check response
+if [[ "$RESP_CODE" != "403" ]]; then
+    echo "Expected 403 got $RESP_CODE"
+fi
